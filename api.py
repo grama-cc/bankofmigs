0b513d004d5c48c1b897a0c623349ceef81a9d8a
Pablo Abdelhay <pabloabdelhay@gmail.com>
2016-09-23 20:01:13 -0300

    config

--- api.py
+++ api.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost:3306/bankofmigs'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class User(db.Model):
