820a2bfa90c9aeb2a7b96187b41b6635b37e01aa
edinhoo <eamedeirosrj@gmail.com>
2016-09-23 18:22:25 -0300

    add first js

---
+++ build/js/script.js
@@ -0,0 +1,31 @@
+var data;
+var user = 'harrison';
+
+$.get( 'data.json', function( data ) {
+  data = JSON.parse( data );
+
+  $.each( data, function() {
+    var tr = $('<tr/>');
+
+    tr.append( $('<td/>').text( convertDate( this.date ) ) );
+    tr.append( $('<td/>').text( this.description ) );
+
+    if ( this.donor == user ) {
+      tr.append( $('<td/>').text( this.value * -1 ) );
+      tr.append( $('<td/>').text( this.receiver ) );
+    } else {
+      tr.append( $('<td/>').text( this.value ) );
+      tr.append( $('<td/>').text( this.donor ) );
+    }
+
+    $( '.transitions' ).append( tr );
+  });
+});
+
+
+function convertDate( timestamp ) {
+  var _dateObj = $.parseJSON( '{"created": ' + timestamp + '}' );
+  var _date = new Date( 1000 * _dateObj.created );
+
+  return _date.toString();
+}
