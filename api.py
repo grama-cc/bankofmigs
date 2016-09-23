5395462dc9e5f768fce1ff8943733b7e762f8309
Pablo Abdelhay <pabloabdelhay@gmail.com>
2016-09-23 18:53:38 -0300

    backend changes

--- api.py
+++ api.py
@@ -1,14 +1,48 @@
-from flask import Flask
+from flask import Flask, request
+from flask.wrappers import Response
+
 app = Flask(__name__)
 
-@app.route("/user/")
-def user():
-    return "Hello World!"
+@app.route("/user/<int:username>")
+def user(user_id):
+    from db import User
+    user = User.query.filter_by(id=user_id).first()
+    return {'id': user.id, 'username': user.username, 'email': user.email, 'balance': user.balance}
 
-@app.route("/transaction/")
+@app.route("/transaction/", methods=['GET', 'POST'])
 def transaction():
-    return "Hello World!"
+    from db import User, Transaction, db
+    if request.method == 'POST':
+        sender_username = request.form.get('sender', None)
+        receiver_username = request.form.get('receiver', None)
+
+        sender = User.query.filter_by(username=sender_username).first()
+        if not sender:
+            sender = User(sender_username)
+        receiver = User.query.filter_by(id=receiver_username).first()
+        if not receiver:
+            receiver = User(receiver_username)
+
+        amount = request.form.get('amount')
+        transaction = Transaction()
+        transaction.sender = sender_username
+        transaction.receiver = receiver_username
+        transaction.value = amount
+        sender.balance -= amount
+        receiver.balance += amount
+
+        db.session.add(sender)
+        db.session.add(receiver)
+        db.session.commit()
+    else:
+        pass
+    return Response(response={}, status=200, mimetype="application/json")
+
+
+@app.route("/transactions/", methods=['GET'])
+def transactions():
+    return "BAL"
 
 if __name__ == "__main__":
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://migs:migs@192.168.1.26/bank-of-migs-dev'
-    app.run()
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost:3306/bankofmigs'
+    app.run(debug=True)
--- db.py
+++ db.py
@@ -6,30 +6,23 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
-    balance = db.Column(db.Float())
+    balance = db.Column(db.Float(), default=0)
 
-    loans = db.relationship('Transaction', backref='sender', lazy='dynamic')
-    debits = db.relantionship('Transaction', backref='receiver', lazy='dynamic')
-
-    def __init__(self, username, email):
+    def __init__(self, username):
         self.username = username
-        self.email = email
 
     def __repr__(self):
         return '<User: %r>' % self.username
 
 class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    sender_id = db.Column(db.Integer, db.ForeignKey('sender.id'))
-    receiver_id = db.Column(db.Integer, db.ForeignKey('receiver.id'))
-    description = db.Column(db.String(120))
+    sender = db.Column(db.String(120))
+    receiver = db.Column(db.String(120))
     date = db.Column(db.DateTime)
-    value = db.Column(db.Float)
+    amount = db.Column(db.Float)
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+    def __init__(self):
+        pass
 
     def __repr__(self):
         return '<User: %r>' % self.username
